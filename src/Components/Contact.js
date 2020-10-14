0cbbad151f937ce9d569331fb97072208a894cfb
lilykr <lisa-lou.k@hotmail.fr>
2020-10-14 11:24:30 +0200

    added state on status

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,23 +2,36 @@ import React from "react";
 import "./contact.css";
 import PropTypes from "prop-types";
 
-const Contact = ({avatar,name, online}) => {
-  const statusIndicator = online ? "status-online" : "status-offline";
-  const status = online ? "Online" : "Offline";
-
-  return (
-    <div className="Contact">
-      <img className="avatar" src={avatar} alt={name} />
-      <div>
-        <h3 className="name">{name}</h3>
-        <div className="status">
-          <div className={statusIndicator} />
-          <p className="status-text">{status}</p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    }
+  }
+  render() {
+      const statusIndicator = this.state.online ? "status-online" : "status-offline";
+      const status = this.state.online ? "Online" : "Offline";
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+        <div>
+          <h3 className="name">{this.props.name}</h3>
+          <div className="status">
+            <div className={statusIndicator} 
+              onClick={(event) => {
+                const newStatus = !this.state.online;
+                this.setState({online: newStatus})
+              }}
+            />
+            <p className="status-text">{status}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 Contact.propTypes = {
   name: PropTypes.string.isRequired,
